f33a598aec315f512799b2b1570e76f73b63c96a
rati <rmekokishvili3gmail.com>
2023-07-30 18:40:46 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+let number1 = prompt ('user');
+let number2 = prompt ('password');
+let axalicipri1 = Number(number1);
+let axalicipri2 = Number(number2);
+alert(`jami ${number1 + number2}`)
+alert(`namravli ${number1 * number2}`)
+
+
+
+
+
+
+
